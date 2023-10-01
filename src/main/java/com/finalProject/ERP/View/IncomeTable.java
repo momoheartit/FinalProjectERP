65078e4f39da890a31607f62816a6de08a5767ce
momoheartit <monika.szfvar@gmail.com>
2023-10-01 20:52:20 +0200

    excelbe mentés működik

--- src/main/java/com/finalProject/ERP/View/IncomeTable.java
+++ src/main/java/com/finalProject/ERP/View/IncomeTable.java
@@ -1,20 +1,32 @@
 package com.finalProject.ERP.View;
 
+import com.finalProject.ERP.Model.Export.IncomeExcel;
 import com.finalProject.ERP.Model.IncomeEntity;
 import com.finalProject.ERP.View.GUI.Table;
 import java.util.List;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 public class IncomeTable extends Table<IncomeEntity> {
 
     public IncomeTable(Pane parent) {
         super(parent);
         
+        // VBox létrehozása
+        VBox vbox = new VBox();
+
+        // Gomb hozzáadása a VBox-hoz
         Button exportButton = new Button("Export to Excel");
         exportButton.setOnAction(event -> exportToExcel());
-        parent.getChildren().add(exportButton);
-        
+        vbox.getChildren().add(exportButton);
+
+        // Táblázat hozzáadása a VBox-hoz
+        vbox.getChildren().add(this);
+
+        // VBox hozzáadása a szülőhöz
+        parent.getChildren().add(vbox);
+
         addColumn("ID", "id", 10);
         addColumn("Partner", "partnerName", 100);
         addColumn("Amount", "amount", 75);
@@ -36,5 +48,7 @@ public class IncomeTable extends Table<IncomeEntity> {
     
     private void exportToExcel(){
         System.out.println("teszem excelbe asszonyom!");
+        List<IncomeEntity> data = getItems();
+        IncomeExcel.exportToExcel(data, "C:/Users/forAndroid/Desktop/IncomeData.xlsx");
     }
 }
